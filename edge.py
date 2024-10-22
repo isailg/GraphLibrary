9e16f68f0e778f8626356354eafbf2cc76377b09
isailg <ilogamx@gmail.com>
2024-10-22 01:19:47 -0400

    Initial commit

---
+++ edge.py
@@ -0,0 +1,10 @@
+import math
+
+class Edge:
+
+	def __init__(self, id, start, end):
+		
+		self.id = id
+		self.start = start
+		self.end = end
+
---
+++ graph.py
@@ -0,0 +1,11 @@
+import math
+
+from node import Node
+from edge import Edge
+
+class Graph:
+	
+	
+	def __init__(self):
+		
+		self.id
