71b247604c46a3418bf5e43f82ada4d6ee3b0aff
isailg <ilogamx@gmail.com>
2024-11-20 00:24:25 -0600

    Revert "Merge pull request #1 from isailg/Proyecto2"
    This reverts commit 834b4f0bc1fd56fd32a625edbda1838f3ff270f8, reversing
    changes made to b37c8e9e200d3bd4f6c1c233deb94da5356735ae.

--- algorithms.py
+++ algorithms.py
@@ -6,202 +6,200 @@ from math import sqrt
 
 
 def grafoMalla(m, n, directed= False):
-    """
-    Genera grafo de malla
-    Entrada:
-        m: número de columnas (>1)
-        n: número de filas (>1)
-        directed: Es dirigido o no
-    Salida:
-        Grafo
-    """
-    graph = Graph()
+	"""
+	Genera grafo de malla
+	Entrada:
+		m: número de columnas (>1)
+		n: número de filas (>1)
+		directed: Es dirigido o no
+	Salida:
+		Grafo
+	"""
+	graph = Graph()
 
-    c=[]
-    index=0
-    for i in range(m):
-        for j in range(n):
-            c=[i,j]
-            graph.addNode(index,c)
-            index = index + 1
-    c=[]
-    index=0
-    for i in range(m):
-        for j in range(n):
-            node = index
-            if (i<=m-2 and j<=n-2):
-                node_right = index + 1
-                node_bottom = (i+1)*n + j
-                graph.addEdge(index,node,node_right)
-                graph.addEdge(index,node,node_bottom)
-                index = index + 1
-                
-            if(j==n-1 and i!=m-1):
-                node_bottom = (i+1)*n + j
-                graph.addEdge(index,node,node_bottom)
-                index = index + 1
-            
-            if(i==m-1 and j!=n-1):
-                node_right = index + 1
-                graph.addEdge(index,node,node_right)
-                index = index + 1
-        
-    
-            
-    return graph
+	c=[]
+	index=0
+	for i in range(m):
+		for j in range(n):
+			c=[i,j]
+			graph.addNode(index,c)
+			index = index + 1
+	c=[]
+	index=0
+	for i in range(m):
+		for j in range(n):
+			node = index
+			if (i<=m-2 and j<=n-2):
+				node_right = index + 1
+				node_bottom = (i+1)*n + j
+				graph.addEdge(index,node,node_right)
+				graph.addEdge(index,node,node_bottom)
+				index = index + 1
+				
+			if(j==n-1 and i!=m-1):
+				node_bottom = (i+1)*n + j
+				graph.addEdge(index,node,node_bottom)
+				index = index + 1
+			
+			if(i==m-1 and j!=n-1):
+				node_right = index + 1
+				graph.addEdge(index,node,node_right)
+				index = index + 1
+		
+	
+			
+	return graph
 
 
 def grafoErdosRenyi(n, m, directed= False):
-    """
-    Genera grafo aleatorio con el modelo Erdos-Renyi
-    Entrada:
-        n: número de nodos (>0)
-        m: número de aristas (>=n-1)
-        directed: Es dirigido o no
-    Salida:
-        Grafo
-    """ 
-    graph = Graph()
-    
-    for i in range (n):
-        graph.addNode(i)
-        
-    for i in range(m):
-        n1 = random.randint(0,n-1)
-        n2 = random.randint(0,n-1)
-        if n1 != n2:
-            graph.addEdge(i,n1,n2)
-            
-    return graph
-        
-        
+	"""
+	Genera grafo aleatorio con el modelo Erdos-Renyi
+	Entrada:
+		n: número de nodos (>0)
+		m: número de aristas (>=n-1)
+		directed: Es dirigido o no
+	Salida:
+		Grafo
+	""" 
+	graph = Graph()
+	
+	for i in range (n):
+		graph.addNode(i)
+		
+	for i in range(m):
+		n1 = random.randint(0,n-1)
+		n2 = random.randint(0,n-1)
+		if n1 != n2:
+			graph.addEdge(i,n1,n2)
+			
+	return graph
+		
+		
 def grafoGilbert(n, p, directed= False):
-    """
-    Genera grafo aleatorio con el modelo Gilbert
-    Entrada:
-        n: número de nodos (>0)
-        p: probabilidad para crear un arista [0,1]
-        directed: Es dirigido o no
-    Salida:
-        Grafo
-    """ 
-    graph = Graph()
-    
-    for i in range(n):
-        graph.addNode(i)
-    c = 0
-    for i in range(n):
-        for j in range(n):
-            if (random.random()<=p):
-                graph.addEdge(c,i,j)
-                c = c + 1
-    return graph
-    
-    
+	"""
+	Genera grafo aleatorio con el modelo Gilbert
+	Entrada:
+		n: número de nodos (>0)
+		p: probabilidad para crear un arista [0,1]
+		directed: Es dirigido o no
+	Salida:
+		Grafo
+	""" 
+	graph = Graph()
+	
+	for i in range(n):
+		graph.addNode(i)
+	c = 0
+	for i in range(n):
+		for j in range(n):
+			if (random.random()<=p):
+				graph.addEdge(c,i,j)
+				c = c + 1
+	return graph
+	
+	
 def grafoGeografico(n, r, directed= False):
-    """
-    Genera grafo aleatorio con el modelo geográfico simple
-    Entrada:
-        n: número de nodos (>0)
-        r: distancia máxima para crear un nodo [0,1]
-        directed: Es dirigido o no
-    Salida:
-        Grafo
-    """
-    graph = Graph()
-    
-    for i in range(n):
-        x = random.random()
-        y = random.random()
-        graph.addNode(i,x,y)
-        
-    c = 0
-    for i in range(n):
-        x1 = graph.nodes[i].x
-        y1 = graph.nodes[i].y
-        for j in range(n):
-            x2 = graph.nodes[j].x
-            y2 = graph.nodes[j].y
-    
-            dist = sqrt((x2-x1)**2+(y2-y1)**2)
-            
-            if (dist<=r):
-                graph.addEdge(c,i,j)
-                c = c + 1
-    return graph
+	"""
+	Genera grafo aleatorio con el modelo geográfico simple
+	Entrada:
+		n: número de nodos (>0)
+		r: distancia máxima para crear un nodo [0,1]
+		directed: Es dirigido o no
+	Salida:
+		Grafo
+	"""
+	graph = Graph()
+	
+	for i in range(n):
+		x = random.random()
+		y = random.random()
+		graph.addNode(i,x,y)
+		
+	c = 0
+	for i in range(n):
+		x1 = graph.nodes[i].x
+		y1 = graph.nodes[i].y
+		for j in range(n):
+			x2 = graph.nodes[j].x
+			y2 = graph.nodes[j].y
+	
+			dist = sqrt((x2-x1)**2+(y2-y1)**2)
+			
+			if (dist<=r):
+				graph.addEdge(c,i,j)
+				c = c + 1
+	return graph
 
 
 
 def grafoBarabasiAlbert(n, d, directed= False):
-    """
-    Genera grafo aleatorio con el modelo Barabasi-Albert
-    Entrada:
-        n: número de nodos (>0)
-        d: grado máximo esperado por cada nodo (>1)
-        directed: Es dirigido o no
-    Salida:
-        Grafo
-    """ 
-    graph = Graph()
-    
+	"""
+	Genera grafo aleatorio con el modelo Barabasi-Albert
+	Entrada:
+		n: número de nodos (>0)
+		d: grado máximo esperado por cada nodo (>1)
+		directed: Es dirigido o no
+	Salida:
+		Grafo
+	""" 
+	graph = Graph()
+	
 
-    graph.addNode(0)
-    c = 2
-    for i in range(1,n):
-        graph.addNode(i)
-        randomNodes = random.sample(range(i),i) 
-        conected = False
-        j=0
-        while (conected == False) and (j < i):
-        #for j in range(i):
-            node = graph.nodes[randomNodes[j]]        
-            deg = graph.getDegree(node)
-            p = 1 - deg / d        
-            
-            
-            if random.random() <= p:
-                if (i!=randomNodes[j]):
-                    graph.addEdge(c,i,randomNodes[j])
-                    c = c + 1
-                    conected = True
-                    
-    return graph
+	graph.addNode(0)
+	c = 2
+	for i in range(1,n):
+		graph.addNode(i)
+		randomNodes = random.sample(range(i),i)
+				
+		for j in range(i):
+
+			node = graph.nodes[randomNodes[j]]		
+			deg = graph.getDegree(node)
+			p = 1 - deg / d		
+			
+			
+			if random.random() <= p:
+				if (i!=randomNodes[j]):
+					graph.addEdge(c,i,randomNodes[j])
+					c = c + 1
+			
+	return graph
 
 def grafoDorogovtsevMendes(n, directed= False):
-    """
-    Genera grafo aleatorio con el modelo Dorogovtsev-Mendes
-    Entrada:
-        n: número de nodos (>=3)
-        directed: Es dirigido o no
-    Salida:
-        Grafo
-    """ 
-    
-    graph = Graph()
-    
+	"""
+	Genera grafo aleatorio con el modelo Dorogovtsev-Mendes
+	Entrada:
+		n: número de nodos (>=3)
+		directed: Es dirigido o no
+	Salida:
+		Grafo
+	""" 
+	
+	graph = Graph()
+	
 
-    for i in range(3):
-        graph.addNode(i)
+	for i in range(3):
+		graph.addNode(i)
 
-    graph.addEdge(0,0,1)
-    graph.addEdge(1,1,2)
-    graph.addEdge(2,2,0)
-    
-    c=3
-    for i in range(3,n):
-        
-        graph.addNode(i)
-        
-        random_edge = random.randint(0,i-1)
-        
-        node1 = graph.edges[random_edge].start
-        node2 = graph.edges[random_edge].end
-        
-        graph.addEdge(c,i,node1)
-        graph.addEdge(c+1,i,node2)
-        
-        c = c + 2
-        
-    
-            
-    return graph
+	graph.addEdge(0,0,1)
+	graph.addEdge(1,1,2)
+	graph.addEdge(2,2,0)
+	
+	c=3
+	for i in range(3,n):
+		
+		graph.addNode(i)
+		
+		random_edge = random.randint(0,i-1)
+		
+		node1 = graph.edges[random_edge].start
+		node2 = graph.edges[random_edge].end
+		
+		graph.addEdge(c,i,node1)
+		graph.addEdge(c+1,i,node2)
+		
+		c = c + 2
+		
+	
+			
+	return graph
--- main.py
+++ main.py
@@ -15,112 +15,24 @@ if __name__ == "__main__":
 	nlist = [30,100,500]
 	
 	for n in nlist:
-		'''
-		gER = grafoErdosRenyi(n,100,False)
-		gER.saveGraphViz(f"GeneratedGraphs(.gv)/ErdosRenyi{n}")
-		
-        
-		"""
-		BFS_ER = gER.BFS(12)
-		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/ErdosRenyi_{n}_BFS")
-		
-		explored_nodes = []
-		DFS_Rtree = Graph()
-		gER.DFS_R(12, explored_nodes, DFS_Rtree)
-		DFS_Rtree.saveGraphViz(f"CalculatedGraphs(.gv)/ErdosRenyi_{n}_DFS_R")
-		DFS_Rtree.clear()
-		explored_nodes.clear()
-		
-		ITree_ER = gER.DFS_I(12)
-		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/ErdosRenyi_{n}_DFS_I")
-		"""
-		
+	
+		gER = grafoErdosRenyi(n,500,False)
+		gER.saveGraphViz(f"ErdosRenyi{n}")
+
 		gGi = grafoGilbert(n,0.3,False)
-		gGi.saveGraphViz(f"GeneratedGraphs(.gv)/Gilbert{n}")
-		
-		"""			
-		BFS_ER = gGi.BFS(12)
-		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Gilbert{n}_BFS")
-		
-		explored_nodes = []
-		DFS_Rtree = Graph()
-		RTree_ER = gGi.DFS_R(12, explored_nodes, DFS_Rtree)
-		RTree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Gilbert{n}_DFS_R")
-		DFS_Rtree.clear()
-		explored_nodes.clear()
-		
-		ITree_ER = gGi.DFS_I(12)
-		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Gilbert{n}_DFS_I")
-		"""	
+		gGi.saveGraphViz(f"Gilbert{n}")
 		
 		gM = grafoMalla(n,int(n/2),False)
-		gM.saveGraphViz(f"GeneratedGraphs(.gv)/Malla{n}")
-		
-		"""	
-		BFS_ER = gM.BFS(12)
-		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Malla{n}_BFS")
-		
-		explored_nodes = []
-		DFS_Rtree = Graph()
-		RTree_ER = gM.DFS_R(12, explored_nodes, DFS_Rtree)
-		RTree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Malla{n}_DFS_R")
-		DFS_Rtree.clear()
-		explored_nodes.clear()
-		
-		ITree_ER = gM.DFS_I(12)
-		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Malla{n}_DFS_I")
-		"""
-		
+		gM.saveGraphViz(f"Malla{n}")
+
 		gGe = grafoGeografico(n,0.6,False)
-		gGe.saveGraphViz(f"GeneratedGraphs(.gv)/GeograficoSimple{n}")
-		
-		"""
-		BFS_ER = gGe.BFS(12)
-		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/GeograficoSimple{n}_BFS")
-		
-		explored_nodes = []
-		DFS_Rtree = Graph()
-		RTree_ER = gGe.DFS_R(12, explored_nodes, DFS_Rtree)
-		RTree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/GeograficoSimple{n}_DFS_R")
-		DFS_Rtree.clear()
-		explored_nodes.clear()
-		
-		ITree_ER = gGe.DFS_I(12)
-		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/GeograficoSimple{n}_DFS_I")
-		"""
-		'''
+		gGe.saveGraphViz(f"GeograficoSimple{n}")
+
 		gBA = grafoBarabasiAlbert(n,6,False)
-		gBA.saveGraphViz(f"GeneratedGraphs(.gv)/BarabasiAlbert{n}")
-		
-		"""	
-		BFS_ER = gBA.BFS(12)
-		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/BarabasiAlbert{n}_BFS")
-		
-		explored_nodes = []
-		DFS_Rtree = Graph()
-		RTree_ER = gBA.DFS_R(12, explored_nodes, DFS_Rtree)
-		RTree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/BarabasiAlbert{n}_DFS_R")
-		DFS_Rtree.clear()
-		explored_nodes.clear()
-		
-		ITree_ER = gBA.DFS_I(12)
-		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/BarabasiAlbert{n}_DFS_I")
-		"""
-		'''
+		gBA.saveGraphViz(f"BarabasiAlbert{n}")
+
 		gDM = grafoDorogovtsevMendes(n,False)
-		gDM.saveGraphViz(f"GeneratedGraphs(.gv)/DorogovtsevMendes{n}")
-		'''
-		"""
-		BFS_ER = gDM.BFS(12)
-		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/DorogovtsevMendes{n}_BFS")
+		gDM.saveGraphViz(f"DorogovtsevMendes{n}")
 		
-		explored_nodes = []
-		DFS_Rtree = Graph()
-		RTree_ER = gDM.DFS_R(12, explored_nodes, DFS_Rtree)
-		RTree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/DorogovtsevMendes{n}_DFS_R")
-		DFS_Rtree.clear()
-		explored_nodes.clear()
-		
-		ITree_ER = gDM.DFS_I(12)
-		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/DorogovtsevMendes{n}_DFS_I")
-		"""
+
+
