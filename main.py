c8d61a699e608d8cfd5bde37a33c18ef455e0500
isailg <ilogamx@gmail.com>
2024-11-24 02:30:09 -0600

    Updated graphs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-"""
-Programa principal
+""" Programa principal
 Ejecuta cada uno de los modelos de generación de grafos para 30,100 y 500 nodos, y exporta su archivo .gv correspondiente.
 Por: Isaí López García
 """
@@ -13,26 +12,26 @@ from algorithms import *
 if __name__ == "__main__":
     
     #Cantidad de nodos para generar grafos
-    nlist = [30]
+    nlist = [30, 100, 500]
 
     for n in nlist:
 
         gER = grafoErdosRenyi(n,500,False)
-        #gER.saveGraphViz(f"ErdosRenyi{n}")
+        gER.saveGraphViz(f"ErdosRenyi{n}")
 
         gGi = grafoGilbert(n,0.3,False)
-        #gGi.saveGraphViz(f"Gilbert{n}")
+        gGi.saveGraphViz(f"Gilbert{n}")
         
         gM = grafoMalla(n,int(n/2),False)
-        #gM.saveGraphViz(f"Malla{n}")
+        gM.saveGraphViz(f"Malla{n}")
 
         gGe = grafoGeografico(n,0.6,False)
-        #gGe.saveGraphViz(f"GeograficoSimple{n}")
+        gGe.saveGraphViz(f"GeograficoSimple{n}")
         
         gBA = grafoBarabasiAlbert(n,6,False)
-        #gBA.saveGraphViz(f"BarabasiAlbert{n}")
+        gBA.saveGraphViz(f"BarabasiAlbert{n}")
         
         gDM = grafoDorogovtsevMendes(n,False)
-        #gDM.saveGraphViz(f"DorogovtsevMendes{n}")
+        gDM.saveGraphViz(f"DorogovtsevMendes{n}")
 
 
