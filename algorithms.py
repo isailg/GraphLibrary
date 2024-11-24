1fdb9fcc7db9540629ac553f36d18fabb64e62d9
isailg <ilogamx@gmail.com>
2024-11-24 02:25:15 -0600

    Updated graphs

--- algorithms.py
+++ algorithms.py
@@ -6,17 +6,21 @@ from math import sqrt
 
 
 def grafoMalla(m, n, directed= False):
-    """
-    Genera grafo de malla
+
+    """  Genera grafo de malla
+
     Entrada:
         m: número de columnas (>1)
         n: número de filas (>1)
         directed: Es dirigido o no
+
     Salida:
         Grafo
     """
+
     graph = Graph()
 
+    #Creando nodos del grafo
     c=[]
     index=0
     for i in range(m):
@@ -24,48 +28,56 @@ def grafoMalla(m, n, directed= False):
             c=[i,j]
             graph.addNode(index,c)
             index = index + 1
+
+    #Creando aristas (conectando nodos)
     c=[]
     index=0
     for i in range(m):
         for j in range(n):
             node = index
+
+            #A la derecha y hacia abajo
             if (i<=m-2 and j<=n-2):
                 node_right = index + 1
                 node_bottom = (i+1)*n + j
                 graph.addEdge(index,node,node_right)
                 graph.addEdge(index,node,node_bottom)
                 index = index + 1
-                
+
+            #A la derecha, última fila
             if(j==n-1 and i!=m-1):
                 node_bottom = (i+1)*n + j
                 graph.addEdge(index,node,node_bottom)
                 index = index + 1
-            
+            #Hacia abajo, última columna
             if(i==m-1 and j!=n-1):
                 node_right = index + 1
                 graph.addEdge(index,node,node_right)
-                index = index + 1
-        
-    
+                index = index + 1    
             
     return graph
 
 
 def grafoErdosRenyi(n, m, directed= False):
-    """
-    Genera grafo aleatorio con el modelo Erdos-Renyi
+
+    """ Genera grafo aleatorio con el modelo Erdos-Renyi
+
     Entrada:
         n: número de nodos (>0)
         m: número de aristas (>=n-1)
         directed: Es dirigido o no
+
     Salida:
         Grafo
     """ 
+
     graph = Graph()
-    
+
+    #Agregando nodos
     for i in range (n):
         graph.addNode(i)
-        
+
+    #Agregando aristas (conecta nodos al azar m veces)
     for i in range(m):
         n1 = random.randint(0,n-1)
         n2 = random.randint(0,n-1)
@@ -76,19 +88,26 @@ def grafoErdosRenyi(n, m, directed= False):
         
         
 def grafoGilbert(n, p, directed= False):
-    """
-    Genera grafo aleatorio con el modelo Gilbert
+
+    """ Genera grafo aleatorio con el modelo Gilbert
+
     Entrada:
         n: número de nodos (>0)
         p: probabilidad para crear un arista [0,1]
         directed: Es dirigido o no
+
     Salida:
         Grafo
     """ 
+
     graph = Graph()
-    
+
+    #Agregando nodos
     for i in range(n):
         graph.addNode(i)
+
+    #Agregando aristas
+    #(Emparejando nodos todos contra todos si cumplen una probabilidad p)
     c = 0
     for i in range(n):
         for j in range(n):
@@ -99,22 +118,27 @@ def grafoGilbert(n, p, directed= False):
     
     
 def grafoGeografico(n, r, directed= False):
-    """
-    Genera grafo aleatorio con el modelo geográfico simple
+
+    """ Genera grafo aleatorio con el modelo geográfico simple
+
     Entrada:
         n: número de nodos (>0)
         r: distancia máxima para crear un nodo [0,1]
         directed: Es dirigido o no
+
     Salida:
         Grafo
     """
+
     graph = Graph()
-    
+
+    #Creando nodos con coordenadas x y 
     for i in range(n):
         x = random.random()
         y = random.random()
         graph.addNode(i,x,y)
-        
+
+    #Agregando aristas si tienen una distancia menor o igual a r
     c = 0
     for i in range(n):
         x1 = graph.nodes[i].x
@@ -122,7 +146,8 @@ def grafoGeografico(n, r, directed= False):
         for j in range(n):
             x2 = graph.nodes[j].x
             y2 = graph.nodes[j].y
-    
+
+            #Obteniendo distancia entre nodos
             dist = sqrt((x2-x1)**2+(y2-y1)**2)
             
             if (dist<=r):
@@ -133,67 +158,81 @@ def grafoGeografico(n, r, directed= False):
 
 
 def grafoBarabasiAlbert(n, d, directed= False):
-    """
-    Genera grafo aleatorio con el modelo Barabasi-Albert
+
+    """ Genera grafo aleatorio con el modelo Barabasi-Albert
+
     Entrada:
         n: número de nodos (>0)
         d: grado máximo esperado por cada nodo (>1)
         directed: Es dirigido o no
+
     Salida:
         Grafo
     """ 
-    graph = Graph()
-    
 
+    graph = Graph()
+
+    #Agregando nodo origen al grafo
     graph.addNode(0)
-    c = 2
+
+    #Creando aristas
     for i in range(1,n):
+
+        #Se crea un nodo
         graph.addNode(i)
+        
+        #Lista de nodos posibles a conectar ordenados de forma aleatoria
         randomNodes = random.sample(range(i),i) 
-        conected = False
+
+        #Conectando nodos solo si cumplen la condición de probabilidad
         j=0
-        while (conected == False) and (j < i):
-        #for j in range(i):
-            node = graph.nodes[randomNodes[j]]        
+        for j in range(i):
+            node = randomNodes[j]    
             deg = graph.getDegree(node)
-            p = 1 - deg / d        
+            p = 1 - (deg / d)        
             
             
             if random.random() <= p:
                 if (i!=randomNodes[j]):
-                    graph.addEdge(c,i,randomNodes[j])
-                    c = c + 1
-                    conected = True
+                    graph.addEdge(i,i,randomNodes[j])
+
+            j = j + 1
                     
     return graph
 
 def grafoDorogovtsevMendes(n, directed= False):
-    """
-    Genera grafo aleatorio con el modelo Dorogovtsev-Mendes
+
+    """ Genera grafo aleatorio con el modelo Dorogovtsev-Mendes
+
     Entrada:
         n: número de nodos (>=3)
         directed: Es dirigido o no
+
     Salida:
         Grafo
     """ 
     
     graph = Graph()
     
-
+    #Creando tres nodos
     for i in range(3):
         graph.addNode(i)
 
+    #Formando el primer triángulo
     graph.addEdge(0,0,1)
     graph.addEdge(1,1,2)
     graph.addEdge(2,2,0)
-    
+
+    #Agregando aristas
     c=3
     for i in range(3,n):
         
         graph.addNode(i)
-        
+
+        #Escogiendo arista al azar
         random_edge = random.randint(0,i-1)
-        
+
+        #Conectando nuevo nodo a los nodos del arista escogido
         node1 = graph.edges[random_edge].start
         node2 = graph.edges[random_edge].end
         
@@ -201,7 +240,5 @@ def grafoDorogovtsevMendes(n, directed= False):
         graph.addEdge(c+1,i,node2)
         
         c = c + 2
-        
-    
             
     return graph
--- graph.py
+++ graph.py
@@ -1,80 +1,76 @@
 import math
-
-
 from node import Node
 from edge import Edge
 
 class Graph:
-	"""
-	Clase Grafo
-		Atributos:
+
+        """ Clase Grafo
+        
+                Atributos:
 			id = Nombre genérico para identificar el grafo
 			nodes = Lista de nodos que conforman el grafo
 			edges = Lista de aristas que componen el grafo
 			str_edges = Lista de aristas que componen el grafo 
 					en formato string(node1 -- node2)
-	"""
-	
-	def __init__(self):
-		
-		self.id = ""
-		self.nodes = []
-		self.edges = []
-		self.str_edges = []
-		
-	def printGraph(self):
-		"""
-		Método para imprimir el grafo en consola
-		"""
-		for i in range(len(self.edges)):
-			print(self.edges[i].id, self.edges[i].start, self.edges[i].end)
-			
-		for i in range(len(self.nodes)):
-			print(self.nodes[i].id, self.nodes[i].x,self.nodes[i].y)
-		
-	def addNode(self,id,x=0,y=0):
-		"""
-		Método para agregar un nodo al grafo
-		"""
-		nod = Node(id,x,y)
-		self.nodes.append(nod)
-	
-	def addEdge(self, id, start, end):
-		"""
-		Método para agregar una arista al grafo
-		"""
-		cur_edge= f"{start}--{end}"
-		curv_edge= f"{end}--{start}"
-		
-		#verifica si el arista existe en el grafo
-		if not( (cur_edge in self.str_edges) or (curv_edge in self.str_edges)):
-						
-			e = Edge(id, start, end)
-			self.edges.append(e)
-			self.str_edges.append(cur_edge)
-			
-	def getDegree(self, node):
-		"""
-		Calcula el grado del nodo
-		"""
-		d=0
-		for edge in self.edges:
-			if ((edge.start == node) or (edge.end == node) ):
-				d = d + 1
+        """
+        
+        def __init__(self):
+                self.id = ""
+                self.nodes = []
+                self.edges = []
+                self.str_edges = []
 
-		return d
-		
-	def saveGraphViz(self, name):
-		"""
-		Exporta un archivo .gv que enumera los aristas del grafo
-		"""
-		named = "GraphViz/"+ name +".gv"
-		with open(named, 'w') as f:
-			f.write("graph " + name +  " {\n")
-			
-			for edge in self.edges:
-				f.write(f"{edge.start} -- {edge.end};\n")
-				
-			f.write("}\n")
+        def printGraph(self):
+                
+                """ Método para imprimir el grafo en consola """
+                
+                for i in range(len(self.edges)):
+                        print(self.edges[i].id, self.edges[i].start, self.edges[i].end)
+                for i in range(len(self.nodes)):
+                        print(self.nodes[i].id, self.nodes[i].x,self.nodes[i].y)
+
+        def addNode(self,id,x=0,y=0):
+                
+                """ Método para agregar un nodo al grafo """
+
+                nod = Node(id,x,y)
+                self.nodes.append(nod)
+
+        def addEdge(self, id, start, end):
+                
+                """ Método para agregar una arista al grafo """
+
+                cur_edge= f"{start}--{end}"
+                curv_edge= f"{end}--{start}"
+                
+		#verifica si el arista existe en el grafo, si no lo agrega
+                if not( (cur_edge in self.str_edges) or (curv_edge in self.str_edges)):
+                        e = Edge(id, start, end)
+                        self.edges.append(e)
+                        self.str_edges.append(cur_edge)
+
+        def getDegree(self, node):
+
+                """ Calcula el grado del nodo """
+
+                d=0
+                for edge in self.edges:
+                        if ((edge.start == node) or (edge.end == node) ):
+                                d = d + 1
+                return d
+
+        def saveGraphViz(self, name):
+
+                """ Exporta un archivo .gv que enumera los aristas del grafo """
+
+                named = "GraphViz/"+ name +".gv"
+                
+                with open(named, 'w') as f:
+                        f.write("graph " + name +  " {\n")
+
+                        for edge in self.edges:
+                                f.write(f"{edge.start} -- {edge.end};\n")
+
+                        f.write("}\n")
 
 		
