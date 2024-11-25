9bafdb51ae4e3b6398360a1348a1f26fbf8cda80
isailg <ilogamx@gmail.com>
2024-11-25 01:13:58 -0600

    Updated classes

--- edge.py
+++ edge.py
@@ -1,17 +1,22 @@
 import math
 
 class Edge:
-	"""
-	Clase Arista
-		Atributos:
-			id = Número entero identificador
-			start = Nodo de inicio del arista
-			end = Nodo final del arista
-	"""
-	def __init__(self, id, start=0, end=0):
-		
-		self.id = id
-		self.start = start
-		self.end = end
-	
+    """ Clase Arista
+
+                Atributos:
+            id = Número entero identificador
+            start = Nodo de inicio del arista
+            end = Nodo final del arista
+                        color = Color del arista
+                        label = Etiqueta del arista
+    """
+        
+    def __init__(self, id, start=0, end=0, color=0, label=""):
+
+                self.id = id
+                self.start = start
+                self.end = end
+                self.color = color
+                self.label = label
+    
 
--- node.py
+++ node.py
@@ -1,17 +1,23 @@
 import math
 
 class Node:
-	"""
-	Clase Nodo
-		Atributos:
-			id = Número entero identificador
-			x = Coordenada x del nodo para usarlo en 
-			    métodos que lo requieran.
-			y = Coordenada y del nodo para usarlo en 
-			    métodos que lo requieran.
-	"""
-	def __init__(self, id, x=0, y=0):
-		
-		self.id = id
-		self.x = x
-		self.y = y
+
+        """ Clase Nodo
+
+                Atributos:
+            id = Número entero identificador
+            x = Coordenada x del nodo para usarlo en 
+                métodos que lo requieran.
+            y = Coordenada y del nodo para usarlo en 
+                métodos que lo requieran.
+                        color = Color del nodo
+                        label = Etiqueta del nodo (string)
+    """
+
+        def __init__(self, id, x=0, y=0, color=0, label=""):
+
+                self.id = id
+                self.x = x
+                self.y = y
+                self.color = color
+                self.label = label
