cbe3486e1d539a1a26cc83ed3f39ec2614a6ea05
isailg <ilogamx@gmail.com>
2024-11-28 00:01:27 -0600

    BFS Correction

--- graph.py
+++ graph.py
@@ -1,71 +1,71 @@
 import math
 import queue
-
 from node import Node
 from edge import Edge
 
 class Graph:
-	"""
-	Clase Grafo
-		Atributos:
-			id = Nombre genérico para identificar el grafo
-			nodes = Lista de nodos que conforman el grafo
-			edges = Lista de aristas que componen el grafo
-			str_edges = Lista de aristas que componen el grafo 
-					en formato string(node1 -- node2)
-	"""
-	
-	def __init__(self):
-		
-		self.id = ""
-		self.nodes = []
-		self.edges = []
-		self.str_edges = []
-		
-	def printGraph(self):
-		"""
-		Método para imprimir el grafo en consola
-		"""
-		for i in range(len(self.edges)):
-			print(self.edges[i].id, self.edges[i].start, self.edges[i].end)
-			
-		for i in range(len(self.nodes)):
-			print(self.nodes[i].id, self.nodes[i].x,self.nodes[i].y)
-		
-	def addNode(self,id,x=0,y=0):
-		"""
-		Método para agregar un nodo al grafo
-		"""
-		nod = Node(id,x,y)
-		self.nodes.append(nod)
-	
-	def addEdge(self, id, start, end):
-		"""
-		Método para agregar una arista al grafo
-		"""
-		cur_edge= f"{start}--{end}"
-		curv_edge= f"{end}--{start}"
-		
-		#verifica si el arista existe en el grafo
-		if not( (cur_edge in self.str_edges) or (curv_edge in self.str_edges)):
-						
-			e = Edge(id, start, end)
-			self.edges.append(e)
-			self.str_edges.append(cur_edge)
-			
-	def getDegree(self, node):
+
+        """ Clase Grafo
+        
+                Atributos:
+            id = Nombre genérico para identificar el grafo
+            nodes = Lista de nodos que conforman el grafo
+            edges = Lista de aristas que componen el grafo
+            str_edges = Lista de aristas que componen el grafo 
+                    en formato string(node1 -- node2)
+        """
+        
+        def __init__(self):
+                self.id = ""
+                self.nodes = []
+                self.edges = []
+                self.str_edges = []
+
+        def printGraph(self):
+                
+                """ Método para imprimir el grafo en consola """
+                
+                for i in range(len(self.edges)):
+                        print(self.edges[i].id, self.edges[i].start, self.edges[i].end)
+                for i in range(len(self.nodes)):
+                        print(self.nodes[i].id, self.nodes[i].x,self.nodes[i].y)
+
+        def addNode(self,id,x=0,y=0):
+                
+                """ Método para agregar un nodo al grafo """
+
+                nod = Node(id,x,y)
+                self.nodes.append(nod)
+
+        def addEdge(self, id, start, end):
+                
+                """ Método para agregar una arista al grafo """
+
+                cur_edge= f"{start}--{end}"
+                curv_edge= f"{end}--{start}"
+                
+                #verifica si el arista existe en el grafo, si no lo agrega
+                if not( (cur_edge in self.str_edges) or (curv_edge in self.str_edges)):
+                        e = Edge(id, start, end)
+                        self.edges.append(e)
+                        self.str_edges.append(cur_edge)
+
+        def getDegree(self, node):
 
                 """ Calcula el grado del nodo """
+
                 d=0
                 for edge in self.edges:
                         if ((edge.start == node) or (edge.end == node) ):
                                 d = d + 1
                 return d
-		
-	def saveGraphViz(self, name):
-                """ Exporta un archivo .gv que enumera los aristas del grafo
-		"""
+
+        def saveGraphViz(self, name):
+
+                """ Exporta un archivo .gv que enumera los aristas del grafo """
+
                 named = name +".gv"
+                
                 with open(named, 'w') as f:
                         f.write("graph " + name +  " {\n")
 
@@ -73,118 +73,119 @@ class Graph:
                                 f.write(f"{edge.start} -- {edge.end};\n")
 
                         f.write("}\n")
-			
-			
-	def clear(self):
-		"""
-		Vacía el grafo
-		"""
-		self.nodes.clear()
-		self.edges.clear()
-		self.str_edges.clear()
-		
-		
-			
-	def neighbors(self, node, neighbors, mark_list):
-		for edge in self.edges:
-			if (edge.start == node and edge.end not in(mark_list)):
-				neighbors.append(edge.end)
-
-			if (edge.end == node and edge.start not in(mark_list)):
-				neighbors.append(edge.start)
-
-		return neighbors
-		
-
-	def BFS(self, s):
-		"""
-		"""
-		BFS_Tree = Graph()
-		
-		q = queue.Queue()
-		explored_nodes = []
-		
-		neighbors = []
-		
-		nlist = []
-		self.neighbors(s, nlist, explored_nodes)
-		q.put(s)
-		
-		
-		i=0
-		while q.empty() != True:
-			node = q.get()
-			print(f"{node}\n")
-			explored_nodes.append(node)
-			neighbors = self.neighbors(node, neighbors, explored_nodes)
-			print(neighbors)
-			for n in neighbors:
-				if edge.end not in(mark_list):
-					e = Edge(i, node, n)
-					print (f"{i} = {node}, {n} \n")
-					BFS_Tree.edges.append(e)
-					q.put(n)
-					i = i + 1
-			neighbors.clear()
-			
-		return BFS_Tree
-		
-		
-		
-		
-		
-	def DFS_R(self, s, explored_nodes, tree):
-		"""
-		"""
-		
-		neighbors = []
-		
-		node = s
-		print(f"{node}\n")
-		
-		explored_nodes.append(node)
-		
-		neighbors = self.neighbors(node, neighbors, explored_nodes)
-		
-		print(neighbors)
-		i=0
-		for n in neighbors:
-			
-			e = Edge(i, node, n)
-			print (f"{i} = {node}, {n} \n")
-			tree.edges.append(e)
-			self.DFS_R(n, explored_nodes, tree)		
-		
-		neighbors.clear()
-			
-		return tree
-		
-				
-	def DFS_I(self, s):
-		"""
-		"""
-		DFS_iTree = Graph()
-		
-		stack = []
-		stack.append(s)
-		
-		explored_nodes = []
-		
-		neighbors = []
-		i=0
-		while len(stack)>0:
-			node = stack.pop()
-			print(f"{node}\n")
-			explored_nodes.append(node)
-			neighbors = self.neighbors(node, neighbors, explored_nodes)
-			print(neighbors)
-			for n in neighbors:
-				e = Edge(i, node, n)
-				print (f"{i} = {node}, {n} \n")
-				DFS_iTree.edges.append(e)
-				stack.append(n)
-				i = i + 1
-			neighbors.clear()
-			
-		return DFS_iTree
         
+        
+        def clear(self):
+            """ Vacía el grafo """
+            self.nodes.clear()
+            self.edges.clear()
+            self.str_edges.clear()
+            
+            
+        def neighbors(self, node, mark_list):
+            """ Encuentra los nodos vecinos de node que no estan marcados como visitados """
+            neighbors = []
+            for edge in self.edges:
+                if (edge.start == node and edge.end not in(mark_list)):
+                    neighbors.append(edge.end)
+
+                if (edge.end == node and edge.start not in(mark_list)):
+                    neighbors.append(edge.start)
+
+            return neighbors
+            
+        
+        def BFS(self, s):
+
+            """ Método para aplicar BFS algorithm al grafo seleccionado
+                    
+                    Entrada:
+                        s = Nodo raíz
+                    
+                    Salida:
+                        BFS_tree = Árbol inducido por BFS Algorithm
+            """
+    
+            BFS_Tree = Graph()
+            q = queue.Queue()
+            
+            marked_nodes = []
+            
+            q.put(s)
+            
+            i=0
+            while q.empty() != True:
+                node = q.get()
+                print(f"{node}\n")
+                marked_nodes.append(node)
+                neighbors = self.neighbors(node, marked_nodes)
+                print(neighbors)
+                for n in neighbors:
+                    if n not in(marked_nodes):
+                        e = Edge(i, node, n)
+                        print (f"{i} = {node}, {n} \n")
+                        BFS_Tree.edges.append(e)
+                        marked_nodes.append(n)
+                        q.put(n)
+                        i = i + 1
+                neighbors.clear()
+                
+            return BFS_Tree
+        
+        def DFS_R(self, s, explored_nodes, tree):
+            """
+            """
+            
+            neighbors = []
+            
+            node = s
+            print(f"{node}\n")
+            
+            explored_nodes.append(node)
+            
+            neighbors = self.neighbors(node, explored_nodes)
+            
+            print(neighbors)
+            i=0
+            for n in neighbors:
+                if n not in(explored_nodes):
+                    e = Edge(i, node, n)
+                    print (f"{i} = {node}, {n} \n")
+                    tree.edges.append(e)
+                    explored_nodes.append(n)
+                    self.DFS_R(n, explored_nodes, tree)
+                    
+            neighbors.clear()
+                
+            return tree
+            
+                    
+        def DFS_I(self, s):
+            """
+            """
+            DFS_iTree = Graph()
+            
+            stack = []
+            stack.append(s)
+            
+            explored_nodes = []
+            
+            neighbors = []
+            i=0
+            while len(stack)>0:
+                node = stack.pop()
+                print(f"{node}\n")
+                explored_nodes.append(node)
+                neighbors = self.neighbors(node, neighbors, explored_nodes)
+                print(neighbors)
+                for n in neighbors:
+                    e = Edge(i, node, n)
+                    print (f"{i} = {node}, {n} \n")
+                    DFS_iTree.edges.append(e)
+                    explored_nodes.append(n)
+                    stack.append(n)
+                    i = i + 1
+                neighbors.clear()
+                
+            return DFS_iTree
+            
--- main.py
+++ main.py
@@ -8,35 +8,42 @@ from node import Node
 from edge import Edge
 from algorithms import *
 
+
 if __name__ == "__main__":
-	
-	
-	nlist = [30,100,500]
-	
-	for n in nlist:
-		
-		gER = grafoErdosRenyi(n,100,False)
-		gER.saveGraphViz(f"GeneratedGraphs(.gv)/ErdosRenyi{n}")
-		    
-		"""
-		BFS_ER = gER.BFS(12)
-		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/ErdosRenyi_{n}_BFS")
-		
-		explored_nodes = []
-		DFS_Rtree = Graph()
-		gER.DFS_R(12, explored_nodes, DFS_Rtree)
-		DFS_Rtree.saveGraphViz(f"CalculatedGraphs(.gv)/ErdosRenyi_{n}_DFS_R")
-		DFS_Rtree.clear()
-		explored_nodes.clear()
-		
-		ITree_ER = gER.DFS_I(12)
-		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/ErdosRenyi_{n}_DFS_I")
-		"""
-		
+
+    #Cantidad de nodos para generar grafos
+    nlist = [100]
+
+    for n in nlist:
+        #Inicializando lista de nodos marcados y árbol inducido para algoritmo recursivo DFS
+        marked_nodes = []
+        recursive_DFS_tree = Graph()
+        
+        #Generación de grafo con algoritmo Erdos Renyi
+        gER = grafoDorogovtsevMendes(n,False)
+        #gER.saveGraphViz(f"GeneratedGraphs(.gv)/ErdosRenyi{n}")
+        #Se escoge automáticamente el nodo raíz para la búsqueda (Un nodo conectado)
+        #s = gER.chooseRoot()
+                
+        #Se aplica algoritmo BFS y se exporta el árbol inducido en archivo .gv
+        #BFS_ER = gER.BFS(12)
+        #BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/GeograficoSimple_{n}_BFS")
+        """
+        gER.DFS_R(12, marked_nodes, recursive_DFS_tree)
+        recursive_DFS_tree.saveGraphViz(f"CalculatedGraphs(.gv)/Malla_{n}_DFS_R")
+        recursive_DFS_tree.clear()
+        marked_nodes.clear()
+        
+        """
+	ITree_ER = gER.DFS_I(12)
+	ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/DorogovtsevMendes_{n}_DFS_I")
+
+        
+        """
 		gGi = grafoGilbert(n,0.3,False)
 		gGi.saveGraphViz(f"GeneratedGraphs(.gv)/Gilbert{n}")
-		
-		"""			
+		"""
+        """			
 		BFS_ER = gGi.BFS(12)
 		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Gilbert{n}_BFS")
 		
@@ -49,12 +56,12 @@ if __name__ == "__main__":
 		
 		ITree_ER = gGi.DFS_I(12)
 		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Gilbert{n}_DFS_I")
-		"""	
-		
+        """
+        """
 		gM = grafoMalla(n,int(n/2),False)
 		gM.saveGraphViz(f"GeneratedGraphs(.gv)/Malla{n}")
-		
-		"""	
+		"""
+        """	
 		BFS_ER = gM.BFS(12)
 		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Malla{n}_BFS")
 		
@@ -68,11 +75,11 @@ if __name__ == "__main__":
 		ITree_ER = gM.DFS_I(12)
 		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/Malla{n}_DFS_I")
 		"""
-		
+        """
 		gGe = grafoGeografico(n,0.6,False)
 		gGe.saveGraphViz(f"GeneratedGraphs(.gv)/GeograficoSimple{n}")
-		
 		"""
+        """
 		BFS_ER = gGe.BFS(12)
 		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/GeograficoSimple{n}_BFS")
 		
@@ -86,11 +93,11 @@ if __name__ == "__main__":
 		ITree_ER = gGe.DFS_I(12)
 		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/GeograficoSimple{n}_DFS_I")
 		"""
-		"""
+        """
 		gBA = grafoBarabasiAlbert(n,6,False)
 		gBA.saveGraphViz(f"GeneratedGraphs(.gv)/BarabasiAlbert{n}")
 		"""
-		"""	
+        """	
 		BFS_ER = gBA.BFS(12)
 		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/BarabasiAlbert{n}_BFS")
 		
@@ -104,11 +111,11 @@ if __name__ == "__main__":
 		ITree_ER = gBA.DFS_I(12)
 		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/BarabasiAlbert{n}_DFS_I")
 		"""
-		
+        """
 		gDM = grafoDorogovtsevMendes(n,False)
 		gDM.saveGraphViz(f"GeneratedGraphs(.gv)/DorogovtsevMendes{n}")
-		
 		"""
+        """
 		BFS_ER = gDM.BFS(12)
 		BFS_ER.saveGraphViz(f"CalculatedGraphs(.gv)/DorogovtsevMendes{n}_BFS")
 		
@@ -121,4 +128,4 @@ if __name__ == "__main__":
 		
 		ITree_ER = gDM.DFS_I(12)
 		ITree_ER.saveGraphViz(f"CalculatedGraphs(.gv)/DorogovtsevMendes{n}_DFS_I")
-		"""
+        """
