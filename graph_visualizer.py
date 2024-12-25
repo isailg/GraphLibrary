5ef201f6bd3f5da2a6f9d2b2407eabe3bf5e3230
isailg <ilogamx@gmail.com>
2024-12-25 02:45:14 -0600

    Display ok

--- graph_visualizer.py
+++ graph_visualizer.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from graph import Graph
 import random
 import math
+import numpy
 
 class App:
     def __init__(self):
@@ -23,29 +24,25 @@ class App:
 
     def on_loop(self):
         pass
+
+    def distance(self,p1,p2):
+        return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
     
     def spring_method(self, graph):
         clock = pygame.time.Clock()
         c1=2
-        c2=50
+        c2=1
         c3=1
-        c4=10
+        c4=0.1
         #Initialize Positions
-        list_posx = []
-        list_posy = []
+        list_pos = []
         for node in graph.nodes:
-            posx = random.randint(30,1250)
-            list_posx.append(posx)
-            posy = random.randint(30,690)
-            list_posy.append(posy)
+            pos = numpy.array([random.randint(30,1250),random.randint(30,690)])
+            list_pos.append(pos)
         self._display_surf.fill('white')
-        fax, fay = 0,0
-        frx,fry = 0,0
 
-        k=0
-        for pos in list_posx:
-            pygame.draw.circle(self._display_surf, (255,100,255),(pos,list_posy[k]), 9)
-            k=k+1
+        for pos in list_pos:
+            pygame.draw.circle(self._display_surf, (255,100,255),pos, 9)
         pygame.display.flip()
 
         while (self._running):
@@ -53,42 +50,56 @@ class App:
                 self.on_event(event)
 
             i=0
+            fa,fr = 0,0
             for node in graph.nodes:
-
-                #Atraction Forces
-                neighbors = graph.neighbors(node)
+                print(list_pos[i][0],list_pos[i][1])                
+                #Attractive Forces
+                neighbors = graph.neighbors(node.id)
+                print(neighbors)
                 for m in neighbors:
-                    d = abs(list_posx[i] - list_posx[m])
-                    fax = fax + c1*math.log10(d/c2)
-                    d = abs(list_posx[i] - list_posx[m])
-                    fay = fay + c1*math.log10(d/c2)
+                    d = max(self.distance(list_pos[i],list_pos[m]),1)
+                    if d!=0:
+                        fa = c1*math.log10(d/c2)
+                        dx = list_pos[i][0]-list_pos[m][0]
+                        dy = list_pos[i][1]-list_pos[m][1]
+                        list_pos[i][0] = list_pos[i][0] + c4*fa*dx/d
+                        list_pos[i][1] = list_pos[i][1] + c4*fa*dy/d
+                    
+                #Repulsive Forces
+                for n in list_pos:
+                    d = max(self.distance(list_pos[i],n),1)
+                    if d != 0:
+                        fr = c3/math.sqrt(d)
+                        dx = list_pos[i][0]-n[0]
+                        dy = list_pos[i][1]-n[1]
+                        #list_pos[i][0] = list_pos[i][0] + c4*fr*dx/d
+                        #list_pos[i][1] = list_pos[i][1] + c4*fr*dy/d
 
-                #Repulsion Forces
-                for n in list_posx:
-                    d = abs(list_posx[i]-n)
-                    if d != 0:
-                        frx = frx + c3/(d*d)
-                for n in list_posy:
-                    d = abs(list_posy[i]-n)
-                    if d != 0:
-                        fry = fry + c3/(d*d)
+                        
                 #calculate the force acting on each vertex
                 #move the vertex c4*(force on vertex)
-                list_posx[i] =+ 30 #c4*(fax-frx)
-                list_posy[i] =+ 30 #c4*(fay-fry)
+                print(list_pos[i][0],list_pos[i][1])
+
+                #list_pos[i][0] = list_pos[i][0] + int(c4*(fax))*
+                
+                #print(fax,fay)
+                #list_posx[i] = #c4*(fax-frx)
+                #list_posy[i] =+ 30 #c4*(fay-fry)
                 i=i+1
             
             self._display_surf.fill('white')
 
+            for edge in graph.edges:
+                pygame.draw.line(self._display_surf, (200,200,200),list_pos[edge.start], list_pos[edge.end],1)
+
             #draw a filled circle for each vertex
-            k=0
-            for pos in list_posx:
-                pygame.draw.circle(self._display_surf,(255,100,255) ,(pos,list_posy[k]), 9)
-                k=k+1
+            for pos in list_pos:
+                pygame.draw.circle(self._display_surf,(66,66,66) ,pos, 9)
+
             pygame.display.update()
             clock.tick(60)
             #draw a straight-line segment for each edge
-        
+
     def on_cleanup(self):
         pygame.quit()
 
