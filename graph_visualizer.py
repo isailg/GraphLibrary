ac3e0cbe45059b98092e5b8957c9b4b33b20c474
isailg <ilogamx@gmail.com>
2024-12-30 15:24:07 -0600

    Spring Layout

--- graph_visualizer.py
+++ graph_visualizer.py
@@ -29,113 +29,67 @@ class App:
         return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
     
     def spring_method(self, graph):
+        WIDTH, HEIGHT = 1280, 720
+        NODE_RADIUS = 10
+        k_attraction = 10  # Constante para fuerzas de atracción
+        k_repulsion = 5000  # Constante para fuerzas de repulsión
+        damping = 0.99  # Factor de amortiguación
+        ideal_length = 10 # Longitud ideal de las aristas
         clock = pygame.time.Clock()
-        c1=2 #attraction
-        c2=1 #lenght
-        c3=1 #repulsion
-        c4=0.1 
         
-        #Initialize Positions
-        list_pos = []
-        for node in graph.nodes:
-            pos = numpy.array([random.randint(30,1250),random.randint(30,690)])
-            list_pos.append(pos)
-        self._display_surf.fill('white')
-
-        for pos in list_pos:
-            pygame.draw.circle(self._display_surf, (66,66,66),pos, 9)
-        pygame.display.flip()
-
-        forces = numpy.zeros((len(graph.nodes),2))
+        nodes = {i: [random.randint(100, 700), random.randint(100, 500)] for i in range(len(graph.nodes))}
+        
+        edges = []
+        for edge in graph.edges:
+            e = (edge.start, edge.end)
+            edges.append(e)
+        
+        forces = {i: [0, 0] for i in range(len(graph.nodes))}
 
         while (self._running):
             for event in pygame.event.get():
                 self.on_event(event)
-
             
-            fa,fr = 0,0
+            if True:# k<100:
+                forces = {i: [0, 0] for i in range(len(graph.nodes))}
 
-            print(forces)
-            """
-            #Repulsive forces
-            for i in range(len(graph.nodes)):
-                for j in range (len(graph.nodes)):
-                    if i!=j:
-                        dx = list_pos[i][0] - list_pos[j][0]
-                        dy = list_pos[i][1] - list_pos[j][1]
-                        d = max(self.distance(list_pos[i], list_pos[j]),1)
-                        if d != 0:
-                            force = c3/d**2
-                            forces[i][0] = forces[i][0] + force * dx/d
-                            forces[i][1] = forces[i][1] + force * dy/d
-            """
-            #Attractive forces
-            for edge in graph.edges:
-                dx = list_pos[edge.start][0] - list_pos[edge.end][0]
-                dy = list_pos[edge.start][1] - list_pos[edge.end][1]
-                d = max(self.distance(list_pos[edge.start], list_pos[edge.end]),1)
-                force = c1*math.log10(d/c2)
-                forces[edge.start][0] = forces[edge.start][0] + force*dx/d
-                forces[edge.start][1] = forces[edge.start][1] + force*dy/d
-                forces[edge.end][0] = forces[edge.end][0] - force*dx/d
-                forces[edge.end][1] = forces[edge.end][1] - force*dy/d
+                # Fuerzas de repulsión
+                for i in nodes:
+                    for j in nodes:
+                        if i != j:
+                            dx = nodes[i][0] - nodes[j][0]
+                            dy = nodes[i][1] - nodes[j][1]
+                            dist = max(self.distance(nodes[i], nodes[j]), ideal_length)
+                            force = k_repulsion / dist**2
+                            forces[i][0] += force * dx / dist
+                            forces[i][1] += force * dy / dist
+                # Fuerzas de atracción (logarítmicas)
+                for edge in edges:
+                    i , j = edge
+                    dx = nodes[j][0] - nodes[i][0]
+                    dy = nodes[j][1] - nodes[i][1]
+                    dist = max(self.distance(nodes[i], nodes[j]), ideal_length)
+                    force = k_attraction * math.log10(dist / ideal_length)
+                    forces[i][0] += force * dx / dist
+                    forces[i][1] += force * dy / dist
+                    forces[j][0] -= force * dx / dist
+                    forces[j][1] -= force * dy / dist
+
+                # Actualizar posiciones de los nodos
+                for i in nodes:
+                    nodes[i][0] += forces[i][0] * damping
+                    nodes[i][1] += forces[i][1] * damping
+                    nodes[i][0] = max(NODE_RADIUS, min(WIDTH - NODE_RADIUS, nodes[i][0]))
+                    nodes[i][1] = max(NODE_RADIUS, min(HEIGHT - NODE_RADIUS, nodes[i][1]))
             
-            """
-            for node in graph.nodes:
-                print(list_pos[i][0],list_pos[i][1])                
-                #Attractive Forces
-                neighbors = graph.neighbors(node.id)
-                print(neighbors)
-                forces = numpy.array([[0,0],[0,0]])
-                for edge in graph.edges:
-                    d = max(self.distance(list_pos[edge.start],list_pos[edge.end]),1)
-                    if d!=0:
-                        fa = c1*math.log10(d/c2)
-                        dx = list_pos[edge.start][0]-list_pos[edge.end][0]
-                        dy = list_pos[edge.start][1]-list_pos[edge.end][1]
-                        forces[1][0] = forces[0] + c4*fa*dx/d
-                        forces[1][1] = forces[1] + c4*fa*dy/d
-            """
-            #Repulsive Forces
-            """
-                for n in list_pos:
-                    d = max(self.distance(list_pos[i],n),1)
-                    if d != 0:
-                        fr = c3/math.sqrt(d)
-                        dx = list_pos[i][0]-n[0]
-                        dy = list_pos[i][1]-n[1]
-                        forces = list_pos[i][0] + c4*fr*dx/d
-                        #list_pos[i][1] = list_pos[i][1] + c4*fr*dy/d
-            """     
-            #calculate the force acting on each vertex
-            #move the vertex c4*(force on vertex)
-            print(forces)
-            print(list_pos)
+            self._display_surf.fill((250,236,255))
             
-            for i in range(len(list_pos)):
-                list_pos[i][0] = list_pos[i][0] + forces[i][0]*c4
-                list_pos[i][1] = list_pos[i][1] + forces[i][1]*c4
-                list_pos[i][0] = max(10, min(1280-10,list_pos[i][0]))
-                list_pos[i][1] = max(10, min(720 - 10, list_pos[i][1]))
-                
-                print(list_pos[i][0],list_pos[i][1])
-
-                #list_pos[i][0] = list_pos[i][0] + int(c4*(fax))*
-                
-                #print(fax,fay)
-                #list_posx[i] = #c4*(fax-frx)
-                #list_posy[i] =+ 30 #c4*(fay-fry)
-            
-            self._display_surf.fill('white')
-
-            for edge in graph.edges:
-                pygame.draw.line(self._display_surf, (200,200,200),list_pos[edge.start],list_pos[edge.end],1)
-
-            #draw a filled circle for each vertex
-            for pos in list_pos:
-                pygame.draw.circle(self._display_surf,(66,66,66) ,pos, 9)
-
-            pygame.display.update()
+            for edge in edges:
+                pygame.draw.line(self._display_surf, (100, 100, 100), nodes[edge[0]], nodes[edge[1]], 1)
+            for i in nodes:
+                pygame.draw.circle(self._display_surf, (8,54,159), (int(nodes[i][0]), int(nodes[i][1])), NODE_RADIUS)
+        
+            pygame.display.flip()
             clock.tick(60)
             #draw a straight-line segment for each edge
 
