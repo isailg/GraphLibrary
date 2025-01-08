d0ad80340d46dd047a10628506976187b1e59347
isailg <ilogamx@gmail.com>
2025-01-07 18:24:20 -0600

    Fruchterman Reingold Layout

--- graph_visualizer.py
+++ graph_visualizer.py
@@ -26,8 +26,17 @@ class App:
     def on_loop(self):
         pass
 
-    def distance(self,p1,p2):
+    def fr(self,k,x):
+        return (k**2)/x
+    
+    def fa(self,k,x):
+        return (x**2) / k
+    
+    def norma(self,p1,p2):
         return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+        
+    def norma(self,dx,dy):
+        return math.sqrt((dx**2)+(dy**2))
     
     def spring_method(self, graph, name):
         #Declaración de constantes
@@ -117,7 +126,120 @@ class App:
             
         #Se guarda el video OpenCV
         out.release()
-
+    
+    def fruchterman_reingold_method(self, graph, name):
+        #Declaración de constantes
+        W, L = 1280, 720
+        vertex_size = 10
+        k = 10
+        t= W/10#0.95
+        a= 20
+        
+        area = W*L
+        
+        
+        #OpenCV para grabar video
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        fps = 60
+        out = cv2.VideoWriter(name+".mp4", fourcc, fps, (W, L))
+        clock = pygame.time.Clock()
+        
+        #Place vertices at random
+        nodes = {i: [random.randint(600, W-600), random.randint(300, L-300)] for i in range(len(graph.nodes))}
+        
+        # Compute optimal pairwase distance
+        #k = math.sqrt(area/(len(nodes)))
+        
+        # Conversión aristas del grafo a listas
+        edges = []
+        for edge in graph.edges:
+            e = (edge.start, edge.end)
+            edges.append(e)
+        
+        # Main Loop
+        while (self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+            
+            f = {i: [0, 0] for i in range(len(graph.nodes))}
+            
+            # Compute repulsive forces
+            for v in nodes:
+                #Se inicializa vector de desplazamientos
+                f[v] = [0,0]
+                for u in nodes:
+                    if (v!=u):
+                        dx = nodes[v][0] - nodes[u][0]
+                        dy = nodes[v][1] - nodes[u][1]
+                        d = self.norma(dx,dy)
+                        if d > 0:
+                            f[v][0] += (dx / d) * self.fr(k,d)
+                            f[v][1] += (dy / d) * self.fr(k,d)
+            # Compute atrattive forces
+            for e in edges:
+                dx = nodes[e[1]][0] - nodes[e[0]][0]
+                dy = nodes[e[1]][1] - nodes[e[0]][1]
+                d = self.norma(dx,dy)
+                if d > 0:
+                    f[e[1]][0] = f[e[1]][0] - (dx/d)*self.fa(k,d)
+                    f[e[1]][1] = f[e[1]][1] - (dy/d)*self.fa(k,d)
+                    f[e[0]][0] = f[e[0]][0] + (dx/d)*self.fa(k,d)
+                    f[e[0]][1] = f[e[0]][1] + (dy/d)*self.fa(k,d)
+            difx,dify = 0,0
+            for v in nodes:
+                # limit max displacement to frame; use temp. t to scale
+                
+                norf = self.norma(f[v][0],f[v][1])
+                if norf > 0:
+                    nodes[v][0] = nodes[v][0] + (f[v][0]/norf)
+                    nodes[v][1] = nodes[v][1] + (f[v][1]/norf)
+                    #nodes[v][0] = min(W/2,max(-W/2,nodes[v][0]))
+                    #nodes[v][1] = min(L/2,max(-L/2,nodes[v][1]))
+                
+                """
+                nodes[v][0] += f[v][0] * min(f[v][0],t)
+                nodes[v][1] += f[v][1] * min(f[v][1],t)
+                nodes[v][0] = max(vertex_size, min(W/2,max( -1*W/2, nodes[v][0])))
+                nodes[v][1] = max(vertex_size, min(L/2,max( -1*L/2, nodes[v][1])))
+                """
+                """
+                difx = difx + (f[v][0]/self.norma(f[v][0],f[v][1]))
+                dify = dify + (f[v][1]/self.norma(f[v][0],f[v][1]))
+                nodes[v][0] += difx
+                nodes[v][1] += dify
+                """
+                nodes[v][0] = max(vertex_size, min(W - vertex_size, nodes[v][0]))
+                nodes[v][1] = max(vertex_size, min(L - vertex_size, nodes[v][1]))
+                
+                
+            #Reduce temperature for next iteration
+            if t < 1:
+                continue
+            t = t - 0.001
+            
+            #Se limpia la pantalla
+            self._display_surf.fill((250,236,255))
+            
+            #Se dibujan aristas en sus nuevas posiciones
+            for edge in edges:
+                pygame.draw.line(self._display_surf, (100, 100, 100), nodes[edge[0]], nodes[edge[1]], 1)
+            
+            #Se dibujan nodos en sus nuevas posiciones
+            for i in nodes:
+                pygame.draw.circle(self._display_surf, (8,54,159), (int(nodes[i][0]), int(nodes[i][1])), vertex_size)
+            
+            #Conversión del pygame frame a OpenCV frame
+            frame = pygame.surfarray.array3d(self._display_surf)
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            frame = cv2.transpose(frame)
+            out.write(frame)
+            
+            pygame.display.flip()
+            clock.tick(60)
+        #Se guarda el video OpenCV
+        out.release()
+        
+        
     def on_cleanup(self):
         pygame.quit()
 
@@ -125,6 +247,7 @@ class App:
         if self.on_init() == False:
             self._running = False
         
-        self.spring_method(graph, name)
+        self.fruchterman_reingold_method(graph, name)
         
         self.on_cleanup()
+
--- main.py
+++ main.py
@@ -14,48 +14,48 @@ import math
 
 if __name__ == "__main__":
     
-    nlist = [100,500]
+    nlist = [100]
     
     # Iniciando Pygame App para visualizar grafos
     Graph_visualizer = App()
     
     for n in nlist:
         
-        """ Erdos Renyi """
+        """ Erdos Renyi 
         #Generacion de grafo
         gER = grafoErdosRenyi(n, int(n/2) ,False)
         gER.saveGraphViz(f"GeneratedGraphs(.gv)/ErdosRenyi{n}")
 
         #Aplicando Metodo Spring para Disposicion de Grafos
-        Graph_visualizer.on_execute(gER, f"ErdosRenyi{n}")
-        
+        Graph_visualizer.on_execute(gER, f"ErdosRenyi{n}_Fruchterman-Reingold")
+        """
                 
-        """ Gilbert """
+        """ Gilbert 
         #Generacion de grafo
         gGi = grafoGilbert(n,0.3,False)
         gGi.saveGraphViz(f"GeneratedGraphs(.gv)/Gilbert{n}")
         
         #Aplicando Metodo Spring para Disposicion de Grafos
         Graph_visualizer.on_execute(gGi, f"Gilbert{n}")
+        """
         
-        
-        """ Malla """
+        """ Malla 
         #Generacion de grafo
         gM = grafoMalla(int(math.sqrt(n)), int(math.sqrt(n))+1,False)
         gM.saveGraphViz(f"GeneratedGraphs(.gv)/Malla{n}")
                 
         #Aplicando Metodo Spring para Disposicion de Grafos
         Graph_visualizer.on_execute(gM, f"Malla{n}")
+        """
         
-        
-        """ Geografico Simple """
+        """ Geografico Simple 
         #Generacion de grafo
         gGe = grafoGeografico(n,0.6,False)
         gGe.saveGraphViz(f"GeneratedGraphs(.gv)/GeograficoSimple{n}")
         
         #Aplicando Metodo Spring para Disposicion de Grafos
         Graph_visualizer.on_execute(gGe, f"Geografico{n}")
-        
+        """
         
         """ Barabasi Albert """
         #Generacion de grafo
@@ -65,11 +65,11 @@ if __name__ == "__main__":
         #Aplicando Metodo Spring para Disposicion de Grafos
         Graph_visualizer.on_execute(gBA, f"BarabasiAlbert{n}")
         
-        """ Dorogovtsev Mendes """
+        """ Dorogovtsev Mendes 
         #Generacion de grafo
         gDM = grafoDorogovtsevMendes(n,False)
         gDM.saveGraphViz(f"GeneratedGraphs(.gv)/DorogovtsevMendes{n}")
         
         #Aplicando Metodo Spring para Disposicion de Grafos
         Graph_visualizer.on_execute(gDM, f"DorogovtsevMendes{n}")
-        
+        """
